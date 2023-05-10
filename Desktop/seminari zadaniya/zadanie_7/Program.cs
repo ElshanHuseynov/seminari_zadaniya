7a406e0b3eeed699e8e2eeaf1e47e5cd055d1e7f
Elshan <beka417@yandex.ru>
2023-05-10 12:51:09 +0300

    Задачи 47, 50, 52

--- Desktop/seminari zadaniya/zadanie_7/Program.cs
+++ Desktop/seminari zadaniya/zadanie_7/Program.cs
@@ -1,84 +1,85 @@
-﻿// //Задача 47
+﻿//Задача 47
 
-// Console.WriteLine("Введите количество строк (m):");
-// int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк (m):");
+int m = int.Parse(Console.ReadLine());
 
-// Console.WriteLine("Введите количество столбцов (n):");
-// int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов (n):");
+int n = int.Parse(Console.ReadLine());
 
-// double[,] array = new double[m, n];
+double[,] array = new double[m, n];
 
-// Random random = new Random();
+Random random = new Random();
 
-// for (int i = 0; i < m; i++)
-// {
-//     for (int j = 0; j < n; j++)
-//     {
-//         array[i, j] = random.NextDouble();
-//     }
-// }
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.NextDouble();
+    }
+}
 
-// Console.WriteLine("Сгенерированный массив:");
-// for (int i = 0; i < m; i++)
-// {
-//     for (int j = 0; j < n; j++)
-//     {
-//         Console.Write(array[i, j] + "\t");
-//     }
-//     Console.WriteLine();
-// }
-// //Задача 50
-// using System;
+Console.WriteLine("Сгенерированный массив:");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+//Задача 50
+using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Введите количество строк (m):");
-//         int m = int.Parse(Console.ReadLine());
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество строк (m):");
+        int m = int.Parse(Console.ReadLine());
 
-//         Console.WriteLine("Введите количество столбцов (n):");
-//         int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов (n):");
+        int n = int.Parse(Console.ReadLine());
 
-//         int[,] array = new int[m, n];
+        int[,] array = new int[m, n];
 
-//         for (int i = 0; i < m; i++)
-//         {
-//             for (int j = 0; j < n; j++)
-//             {
-//                 Console.WriteLine("Введите значение для элемента [{0}, {1}]:", i, j);
-//                 array[i, j] = int.Parse(Console.ReadLine());
-//             }
-//         }
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine("Введите значение для элемента [{0}, {1}]:", i, j);
+                array[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
 
-//         Console.WriteLine("Введенный массив:");
-//         for (int i = 0; i < m; i++)
-//         {
-//             for (int j = 0; j < n; j++)
-//             {
-//                 Console.Write(array[i, j] + "\t");
-//             }
-//             Console.WriteLine();
-//         }
+        Console.WriteLine("Введенный массив:");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
 
-//         Console.WriteLine("Введите позицию элемента для получения его значения:");
-//         Console.WriteLine("Введите индекс строки:");
-//         int rowIndex = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите позицию элемента для получения его значения:");
+        Console.WriteLine("Введите индекс строки:");
+        int rowIndex = int.Parse(Console.ReadLine());
 
-//         Console.WriteLine("Введите индекс столбца:");
-//         int colIndex = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите индекс столбца:");
+        int colIndex = int.Parse(Console.ReadLine());
 
-//         if (rowIndex >= 0 && rowIndex < m && colIndex >= 0 && colIndex < n)
-//         {
-//             int element = array[rowIndex, colIndex];
-//             Console.WriteLine("Значение элемента [{0}, {1}]: {2}", rowIndex, colIndex, element);
-//         }
-//         else
-//         {
-//             Console.WriteLine("Указанный элемент не существует в массиве.");
-//         }
-//     }
-// }
+        if (rowIndex >= 0 && rowIndex < m && colIndex >= 0 && colIndex < n)
+        {
+            int element = array[rowIndex, colIndex];
+            Console.WriteLine("Значение элемента [{0}, {1}]: {2}", rowIndex, colIndex, element);
+        }
+        else
+        {
+            Console.WriteLine("Указанный элемент не существует в массиве.");
+        }
+    }
+}
+
 //Задача 52
 
 using System;
