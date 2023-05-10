03da46c68352570905bf831b390cfe946bc6bf62
Elshan <beka417@yandex.ru>
2023-05-10 12:32:04 +0300

    Двумерный массив заполненный случайнами числами

---
+++ Desktop/seminari zadaniya/zadanie_7/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк (m):");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов (n):");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+Random random = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.NextDouble();
+    }
+}
+
+Console.WriteLine("Сгенерированный массив:");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
