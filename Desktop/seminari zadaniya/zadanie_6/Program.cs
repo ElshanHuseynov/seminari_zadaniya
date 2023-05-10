6a460d182b5b0d24eb81885185666fb1d01afe3b
Elshan <beka417@yandex.ru>
2023-05-10 12:14:17 +0300

    Точка пересечения двух прямых

--- Desktop/seminari zadaniya/zadanie_6/Program.cs
+++ Desktop/seminari zadaniya/zadanie_6/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Введите количество чисел: ");
+﻿// //Пользователь вводит с клавиатуры M чисел. 
+// //Посчитайте, сколько чисел больше 0 ввёл пользователь.
+using System;
+
+Console.Write("Введите количество чисел: ");
 int m = int.Parse(Console.ReadLine());
 
 int count = 0;
@@ -18,3 +22,24 @@ Console.WriteLine($"Количество чисел больше нуля: {coun
 
 Console.WriteLine("Нажмите любую клавишу для выхода...");
 Console.ReadKey();
+
+
+//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.WriteLine("Введите значения k1, b1, k2 и b2:");
+
+double k1 = double.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("Точка пересечения: ({0}, {1})", x, y);
+
+
+
+    
