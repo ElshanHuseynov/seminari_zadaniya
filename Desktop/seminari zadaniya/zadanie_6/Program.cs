505bedf38e61e33b1494505b156167fcb7fbc0ce
Elshan <beka417@yandex.ru>
2023-05-10 11:53:01 +0300

    Больше нуля

---
+++ Desktop/seminari zadaniya/zadanie_6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше нуля: {count}");
+
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey();
