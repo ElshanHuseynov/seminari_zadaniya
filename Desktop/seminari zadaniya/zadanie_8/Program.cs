a94a4848394e02173d9ef09820b4cd30ba52394a
Elshan <beka417@yandex.ru>
2023-05-23 13:29:55 +0300

    Элементы по убыванию двумерного массива

--- Desktop/seminari zadaniya/zadanie_8/Program.cs
+++ Desktop/seminari zadaniya/zadanie_8/Program.cs
@@ -1,82 +1,43 @@
 ﻿// Задача 54
 
-// class Program
-// {    
-//     static void Main()
-//     {
-//         int[,] array = 
-//         {
-//             { 1, 4, 7, 2 },
-//             { 5, 9, 2, 3 },
-//             { 8, 4, 2, 4 }
-//         };
-
-//         int rows = array.GetLength(0);
-//         int cols = array.GetLength(1);
-
-//         for (int i = 0; i < rows; i++)
-//         {
-//             int[] tempArray = new int[cols];
-//             for (int j = 0; j < cols; j++)
-//             {
-//                 tempArray[j] = array[i, j];
-//             }
-
-//             Array.Sort(tempArray);
-//             Array.Reverse(tempArray);
-
-//             for (int j = 0; j < cols; j++)
-//             {
-//                 array[i, j] = tempArray[j];
-//             }
-//         }
-
-//         for (int i = 0; i < rows; i++)
-//         {
-//             for (int j = 0; j < cols; j++)
-//             {
-//                 Console.Write(array[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
-//     }
-// }
-
-
-// Задача 56
 class Program
-{
+{    
     static void Main()
     {
         int[,] array = 
         {
             { 1, 4, 7, 2 },
             { 5, 9, 2, 3 },
-            { 8, 4, 2, 4 },
-            { 5, 2, 6, 7 }
+            { 8, 4, 2, 4 }
         };
 
         int rows = array.GetLength(0);
         int cols = array.GetLength(1);
 
-        int minRowSum = int.MaxValue;
-        int minRowIndex = -1;
-
         for (int i = 0; i < rows; i++)
         {
-            int rowSum = 0;
+            int[] tempArray = new int[cols];
             for (int j = 0; j < cols; j++)
             {
-                rowSum += array[i, j];
+                tempArray[j] = array[i, j];
             }
 
-            if (rowSum < minRowSum)
+            Array.Sort(tempArray);
+            Array.Reverse(tempArray);
+
+            for (int j = 0; j < cols; j++)
             {
-                minRowSum = rowSum;
-                minRowIndex = i;
+                array[i, j] = tempArray[j];
             }
         }
 
-        Console.WriteLine("Номер строки с наименьшей суммой элементов: " + minRowIndex);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
