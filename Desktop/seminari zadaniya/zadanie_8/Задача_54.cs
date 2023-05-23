d23242b7516551576be9b738e84c438fe6ac8475
Elshan <beka417@yandex.ru>
2023-05-23 12:53:51 +0300

    Элементы по убыванию двумерного массива

---
+++ Desktop/seminari zadaniya/zadanie_8/Задача_54.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{    
+    static void Main()
+    {
+        int[,] array = 
+        {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            int[] tempArray = new int[cols];
+            for (int j = 0; j < cols; j++)
+            {
+                tempArray[j] = array[i, j];
+            }
+
+            Array.Sort(tempArray);
+            Array.Reverse(tempArray);
+
+            for (int j = 0; j < cols; j++)
+            {
+                array[i, j] = tempArray[j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
